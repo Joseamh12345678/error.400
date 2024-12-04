b09b5cbf0c67520a8bdfdf52660c1276459b657b
Jose <utm23090705@utma.edu.mx>
2024-12-04 08:43:14 -0600

    wrong

---
+++ backend/controllers/juegos.controllers.js
@@ -0,0 +1,10 @@
+import { modelo } from "../models/alumnos.model";
+
+modelo.create({
+    Name:"Rodolfo",
+    edad:25
+})
+
+const test = ()=>{
+    console.log("Funciona el controlador")
+}
---
+++ backend/models/juegos.js
@@ -0,0 +1,7 @@
+import schema from "mongoose";
+
+const Schema = new Schema({
+    name: String,
+    tipo: Number
+})
+export const modelo = new model("Tabla de alumnos", Esquemas)
